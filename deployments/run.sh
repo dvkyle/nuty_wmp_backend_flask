2ff6563be4af45a98b7c86b7b2013dda31631426
Kyle Zhao <kylez@arfront.com>
2020-09-15 18:59:26 +0800

    update run.sh

--- deployments/run.sh
+++ deployments/run.sh
@@ -3,6 +3,6 @@
 /etc/init.d/nginx start
 service redis-server restart
 
-cd src/wmp_backend/
+cd /home/src/wmp_backend/
 ./wmp_backend_api.sh
 
--- wmp_backend/wmp_backend_api.py
+++ wmp_backend/wmp_backend_api.py
@@ -585,7 +585,7 @@ if __name__ == '__main__':
         print (hooks)
         setup_hooks(hooks)
 
-    app.run(host="127.0.0.1", port=4100, debug=True)
+    app.run(host="0.0.0.0", port=4100, debug=True)
 
     # support websocket for client
     # from geventwebsocket.handler import WebSocketHandler
